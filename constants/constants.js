6ee3f8386551853000fb74f2f97d3142fcf8e7b2
shrikrushana <papupingale123@gmail.com>
2022-10-08 15:49:53 +0530

    url

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,6 @@
 var URL, LOCALURL, IMAGEURL;
-var LOCAL_HOST = "http://localhost:3000/";
-// var LOCAL_HOST = "https://www.eventrio.events/";
+// var LOCAL_HOST = "http://localhost:3000/";
+var LOCAL_HOST = "https://movies-beta-seven.vercel.app/";
 var DEV_HOST = "localhost";
 
 URL = "https://movie-task.vercel.app/";
